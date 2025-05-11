61ff32e04aac967da6ea1d0e26b60b6b9ed75911
lyxal <36217120+Lyxal@users.noreply.github.com>
2025-05-11 16:30:38 +1000

    Going to try a fresh scala layout

--- valiance/common/bytecode/Chunk.scala
+++
@@ -1,6 +0,0 @@
-
-class Chunk:
-    var code: Seq[Byte] = Seq()
-    def init(): Unit = code.empty
-    def writeByte(byte: Byte): Unit = code :+= byte
-    def writeByte(opcode: Opcode): Unit = code :+= opcode.byte
--- valiance/common/bytecode/Opcode.scala
+++
@@ -1,4 +0,0 @@
-enum Opcode(val byte: Byte, val name: String):
-    case PUSH_B1 extends Opcode(0x11, "PUSH_B1")
-    case PUSH_B2 extends Opcode(0x12, "PUSH_B2")
-    case ADD extends Opcode(0x20, "ADD")
